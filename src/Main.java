dd939dadc09edd5b88c589fae9bf4c9a4d0871d9
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-02-26 12:17:51 +0100

    refactor Main

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import utils.Logger;
 
 public class Main {
     public static void main(String[] args){
-        Logger l = new Logger();
         Person p = new Person("Bob", 33);
         Logger.logPrintString("Hello " + p);
     }
